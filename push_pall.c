0499895da8d1407e0c978fd0f1977fac54a346c0
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-16 05:38:22 -0800

    pall func

--- push_pall.c
+++ push_pall.c
@@ -22,3 +22,25 @@ void append_to_stack(stack_t **nnode, __attribute__((unused))unsigned int lnum)
 	head->next = tmpy;
 	tmpy->prev = head;
 }
+
+/**
+ * pall_stack - prints stack values
+ * @top_stack: pointer to top stack
+ * @lnum: line number to opcode
+ */
+void pall_stack(stack_t **top_stack, unsigned int lnum)
+{
+	stack_t *holder;
+
+	(void) lnum;
+	if (tp_stack == NULL)
+		exit(EXIT_FAILURE);
+
+	holder = *top_stack;
+
+	while (holder != NULL)
+	{
+		printf("%d\n", holder->n);
+		holder = holder->next;
+	}
+}
