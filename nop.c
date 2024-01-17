eee84f68dc66cc59b2e89a71ebcf59f8fda508f9
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-17 01:17:07 -0800

    fixed betty

--- nop.c
+++ nop.c
@@ -2,7 +2,7 @@
 
 /**
  * no_work - does nothing
- * @top_stack pointer to top node of stack
+ * @top_stack: pointer to top node of stack
  * @line_no: line number
  */
 void no_work(stack_t **top_stack, unsigned int line_no)
